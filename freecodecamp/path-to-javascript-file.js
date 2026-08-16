4cb31baf0ba39fc7e86d7fd23e43ae30473e4de8
Duyen Dong Ky Nguyen <nguyendongkyduyen308@gmail.com>
2026-08-16 21:10:29 +0700

    learning about accessible

--- freecodecamp/path-to-javascript-file.js
+++ freecodecamp/path-to-javascript-file.js
@@ -11,3 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
     paraEl.textContent = "The game has started!!!";
   });
 });
+
+const passwordEl = document.getElementById("password");
+const passwordHelpText = document.getElementById("password-help");
+
+passwordEl.addEventListener("input", (e) => {
+  const userInput = e.target.value;
+  passwordHelpText.style.color = userInput.length >= 8 ? "green" : "red";
+});
